8097ddedaa7875dc60db68bdaaef6b5c00bd9e3a
standardgalactic <standardgalactic@protonmail.com>
2025-05-19 04:32:28 -0300

    Improved Readability

--- update-fonts.sh
+++ update-fonts.sh
@@ -1,4 +1,4 @@
-for dir in Haplopraxis abraxas arcanum backward-compatibility calculator ensign library quadrivium sitemap standardgalactic.github.io; do
+for dir in Haplopraxis spherepop abraxas arcanum backward-compatibility calculator ensign library quadrivium sitemap standardgalactic.github.io; do
   echo "Processing $dir..."
   cd "$dir" || { echo "Failed to enter $dir"; continue; }
 
@@ -11,7 +11,7 @@ for dir in Haplopraxis abraxas arcanum backward-compatibility calculator ensign 
 
   # Commit and push changes
   git add fonts/
-  git commit -m "Terminal Friendly Font"
+  git commit -m "Improved Readability"
   git push
 
   cd ..
